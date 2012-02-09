6d3a8d118a53e82670c58806a1ae2c8913a4c0b6
Ulises Mendoza <garbage_collector@ymail.com>
2012-02-09 11:10:22 -0600

    abc alumno

---
+++ web/Catalogos/Alumnos/alumnos.js
@@ -0,0 +1,110 @@
+var accion = '';
+//var id_grupo = null;
+var no_expediente_ant;
+
+function validaNombre(grupo){
+  var patron = /[A-Za-zñÑÁÉÍÓÚáéíóúäëïöü\s]/;
+  return !patron.test(grupo);
+}
+         
+$(function(){
+  $( '#forma-agrega-alumno' ).dialog({
+    autoOpen: false,
+    height: 300,
+    width: 350,
+    modal: true,
+    buttons: {
+      'Guardar':function(){                         
+        var noExpediente = $('#noExpediente').val();
+        var nombre = $('#nombre').val();
+        var sexo = $('.radio_sexo:checked').val();
+        var mensaje = '';
+        if (noExpediente == ''){
+          mensaje = mensaje + 'Debe introducir un número de expediente\n';
+        }
+        if ( isNaN(noExpediente)){
+          mensaje = mensaje + 'El número de expediente no es válido\n';
+        }
+        if (nombre == ''){
+          mensaje = mensaje + 'Debe introducir un nombre\n';
+        }
+        if(validaNombre(nombre)){
+          mensaje = mensaje + 'El nombre no es válido\n'
+        }
+        if (sexo == undefined){
+          mensaje = mensaje + 'Debe introducir un sexo\n';
+        }
+        if (mensaje != ''){
+          alert(mensaje);
+        }else{
+          var parameters={};
+          parameters.noExpedienteAnt = no_expediente_ant;
+          parameters.noExpediente=noExpediente;
+          parameters.nombre=nombre;
+          parameters.sexo=sexo;
+          
+          if(accion =='modificar'){
+            //parameters.id_grupo=id_grupo;
+            $.post('modificarAlumno', parameters, 
+              function(data){
+                $('#_principal').load('mostrarAlumnos');
+              },'text');
+          }else{
+            $.post('agregarAlumno', parameters, 
+            function(data){
+              $('#_principal').load('mostrarAlumnos');
+            },'text');
+          }
+          $( this ).dialog( 'close' );
+        } 
+      }//function
+    }//button,
+  });
+  
+  $('#btn-agrega-alumno').click(function(){
+    accion = 'agregar';
+    $('#forma-agrega-alumno').dialog({               
+      title: 'Agregar alumno'
+    });
+    $('#forma-agrega-alumno').dialog('open');
+    $('#noExpediente').val("");
+    $('#nombre').val("");
+    $('#sexoF').attr('checked', false);
+    $('#sexoM').attr('checked', false);
+  });
+  
+  $('#btn-borra-alumno').click(function(){
+    var checks = $('.check_alumno:checked');
+    for(var i =0 ; i<checks.length ; i++){
+      var parameters={};
+      parameters.noExpediente = checks[i].value;
+      $.post('borrarAlumno', parameters, 
+      function(data){
+        $('#_principal').load('mostrarAlumnos');
+      },'text'   );
+    }
+  });
+ 
+  $('#btn-cambia-alumno').click(function(){
+    accion = 'modificar';
+    $('#forma-agrega-alumno').dialog({               
+      title: 'Modificar alumno'
+    });
+    var check = $('.radio_alumno:checked');
+    //id_grupo = check[0].value;
+    $('#forma-agrega-alumno').dialog('open');
+    var noExpediente = $('.radio_alumno:checked').parent().siblings("#input_noExpediente").text();
+    no_expediente_ant = noExpediente;
+    var nombre = $('.radio_alumno:checked').parent().siblings("#input_nombre").text();
+    var sexo = $('.radio_alumno:checked').parent().siblings("#input_sexo").text();
+    $('#noExpediente').val(noExpediente);
+    $('#nombre').val(nombre);
+    if (sexo == 'F'){
+      $('#sexoF').attr('checked', true);
+    }else{
+      $('#sexoM').attr('checked', true);
+    }
+    
+  });
+  
+});
---
+++ web/Catalogos/Alumnos/alumnos.jsp
@@ -0,0 +1,64 @@
+<%-- 
+    Document   : alumnos
+    Created on : 7/02/2012, 11:53:42 PM
+    Author     : ulises
+--%>
+
+ <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+ <script type="text/javascript" src="Catalogos/Alumnos/alumnos.js" />
+   
+<center>
+   <table id="tabla-alumnos-actuales">
+        <thead>
+            <th colspan="6">Alumnos actuales</th>
+        </thead>
+        <tbody>
+            <tr>
+                <th>No. Expediente</th>
+                <th>Nombre</th>
+                <th>Sexo</th>
+                <th>Borrar</th>
+                <th>Modificar</th>
+            </tr>
+            <jsp:useBean id="alumnos" class="java.util.ArrayList" scope="request" />
+            <c:forEach var="alumno" items="${alumnos}">
+               <tr>
+                <td class="resultado" id="input_noExpediente">${alumno.noExpediente}</td>
+                <td class="resultado" id="input_nombre">${alumno.nombre}</td>
+                <td class="resultado" id="input_sexo">${alumno.sexo}</td>
+                <td class="centrado">
+                    <input class="check_alumno" type="checkbox" name="borrar"  value="${alumno.noExpediente}"/>
+                </td>
+                <td class="centrado">
+                    <input class="radio_alumno" type="radio" name="modificar" value="${alumno.noExpediente}"/>
+                </td>
+               </tr> 
+            </c:forEach>
+    
+        </tbody>
+        
+    </table>
+    
+    <button class="ui-button" id="btn-agrega-alumno">Nuevo alumno</button> 
+    <button class="ui-button" id="btn-borra-alumno">Borra alumno</button> 
+    <button class="ui-button" id="btn-cambia-alumno">Modifica alumno</button> 
+    
+      <div id="forma-agrega-alumno" >
+        <form>
+            <fieldset >
+               <div class="div-izquierdo">
+                  <p class="etiqueta"><label>No. expediente: </label>
+                  <p class="etiqueta"><label>Nombre: </label>
+                  <p class="etiqueta"><label>Sexo: </label>
+               </div>
+               <div>
+                 <p><input type="text" id="noExpediente" size="10" maxlength="20"/>
+                 <p><input type="text" id="nombre" size="30" maxlength="45"/>
+                 <p><input class="radio_sexo" id="sexoF" type="radio" name="sexo"  value="F"/><label>Femenino</label>
+                 <input class="radio_sexo" id="sexoM" type="radio" name="sexo" value="M"/><label>Masculino</label>
+               </div>
+            </fieldset>
+       
+        </form>   
+      </div>    
