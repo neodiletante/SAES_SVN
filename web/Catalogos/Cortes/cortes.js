363df3622318bd7ca9b86edc622b9b82fe2d4f6d
Ulises Mendoza <garbage_collector@ymail.com>
2012-02-19 15:09:57 -0600

    cortes

--- web/Catalogos/Cortes/cortes.js
+++ web/Catalogos/Cortes/cortes.js
@@ -1,54 +1,203 @@
          $(function(){
          
-             
+         function limpiar(){
+            $("#fecha_ini").val('');
+            $("#fecha_fin").val('');
+            $("#descripcion").val('');
+            $("#opcion").val('');
+            $("#nuevo-corte").val('');
+            $("#lblcorte").val('');
+         }  
          
+          function setValores(param, funcion){
+            $("#fecha_ini").val(param.fecha_ini);
+            $("#fecha_fin").val(param.fecha_fin);
+            $("#descripcion").val(param.descripcion);
+            $("#opcion").val(funcion);
+            $("#nuevo-corte").val(param.corte);
+            $("#lbl_corte").text(param.corte);
+         } 
+         
+         function getValores(corte){
+             console.log('here in valores');
+             var param={};
+             param.corte=corte;
+             param.fecha_ini=$('#fecha_ini_'+corte).val();
+             param.fecha_fin=$('#fecha_fin_'+corte).val();
+             param.descripcion=$('#descripcion_'+corte).val();
+             param.accion=$('#opcion').val();
+             return param;
+         }
+         
+         function validar(){
+             
+             var max_fecha_fin_d = $('#max_fecha_fin_d').val();
+             var max_fecha_fin_m = $('#max_fecha_fin_m').val();
+             var max_fecha_fin_y = $('#max_fecha_fin_y').val();
+             var max_fecha_fin = new Date(max_fecha_fin_y, max_fecha_fin_m - 1, max_fecha_fin_d );
+             var fecha_fin = $('#fecha_fin').datepicker("getDate");
+             var fecha_ini = $('#fecha_ini').datepicker("getDate");
+             var opcion=$('#opcion').val();
+             
+             console.log('La fecha final del ultimo corte es ' + max_fecha_fin);
+             console.log('La fecha final a validar es ' + fecha_fin);
+             console.log('La fecha inicial a validar es ' + fecha_ini);
+             
+             if ( (fecha_ini <= max_fecha_fin) && opcion=='agregar'){ //al modificar no lo valida
+                alert('El inicio de periodo debe ser posterior al fin del ultimo periodo');     
+                return false;
+             }
+             
+             if (fecha_ini >= fecha_fin){
+                alert('El fin de periodo debe ser posterior a la fecha de inicio');
+                return false;
+             }
+             return true;
+             /*
+             return true;
+             
+             var corte_ant = $("#nuevo-corte").val()-1;
+             var corte_anterior=($("#fecha_fin_"+corte_ant).val() < $("#fecha_ini").val());
+             var valida_fechas=( $("#fecha_ini").val()>$("#fecha_fin").val() );
+             var validar=!(corte_anterior || valida_fechas);
+             if(!validar){
+              if(corte_anterior) 
+                alert('El inicio de periodo debe ser posterior al fin del ultimo periodo');
+              else
+                alert('El fin de periodo debe ser posterior a la fecha de inicio');
+             }
+             return validar;
+             */
+         }
+         
+         function getParameters(){
+            console.log('here in Parameters');
+            var parame={};
+            parame.fecha_ini=$("#fecha_ini").val();
+            parame.fecha_fin=$("#fecha_fin").val();
+            parame.descripcion=$("#descripcion").val();
+            parame.accion=$("#opcion").val();
+            console.log('en getParameters #opcion.val: ' + $("#opcion").val());
+            parame.corte=$("#nuevo-corte").val();
+            return parame;
+         }
+         
+         function agregar(){
+            var param = getParameters();
+            if(param.accion=='agregar' && validar()){ 
+                $.post('Catalogos/Cortes/cortes.jsp', param, function(data){
+                //agregar la fila
+                console.log('Agregando: '+data);
+                   if (data.indexOf('Agregado')){
+                       var fila='<tr id="corte_'+param.corte+
+                           '"><td class="resultado">'+param.corte+
+                           '</td><td class="resultado">'+param.fecha_ini+
+                           '</td><td class="resultado">'+param.fecha_fin+
+                           '</td><td class="resultado">'+param.descripcion+
+                           '</td><td class="centrado"><input type="checkbox" name="corte" value='+param.corte+'></td>'+
+                           '</td><td class="centrado"><input type="radio" name="modificar" value='+param.corte+'></td>'+
+                           '</tr></table>';
+                       //escribir fila
+                       $('#info-cortes tbody').append(fila);
+                       $('#nuevo-corte').val($('#nuevo-corte').val()+1);
+                       $('#forma-agrega-corte' ).dialog( 'close' );
+                       /*
+                       var options = {to: {width: 200, height: 60}};
+                       $( this ).hide( 'slide', options, 1000, function(){} );
+                       */
+                   }
+                   //avisar 
+                   alert(data); 
+                },'text'   );
+                $('#frmCorte').action='?o=11';
+                $('#frmCorte').submit();
+           // },'json'
+            }//else
+         }
+         
+         function modificar(){
+             console.log('voy a modificar');
+             var p = getParameters();
+              console.log('obtuve los parametros para modificar');
+              if(p.accion=='modificar' && validar()){ 
+                    console.log('modificacion validada');
+                  $.post('Catalogos/Cortes/cortes.jsp', p, function(data){
+                     
+                     console.log('llegue al success del modificar');
+                     
+                     if (data.indexOf('modificado')){
+                        //Cambiar en la tabla
+                        var fila='<td class="resultado">'+p.corte+
+                           '</td><td class="resultado">'+p.fecha_ini+
+                           '</td><td class="resultado">'+p.fecha_fin+
+                           '</td><td class="resultado">'+p.descripcion+
+                           '</td><td class="centrado"><input type="checkbox" name="corte" value='+p.corte+'></td>'+
+                           '</td><td class="centrado"><input type="radio" name="modificar" value='+p.corte+'></td>';
+                        
+                            console.log('La fila que voy a cambiar es:' + '#corte_'+p.corte);
+                            $('#corte_'+p.corte).empty();
+                            $('#corte_'+p.corte).append($(fila));
+                            /*
+                        $('#corte_'+p.corte).replaceWith(fila)  ;
+                        $('#corte_'+p.corte).html(fila) ;
+                        $('#corte_'+p.corte).text(fila) ;
+                          */
+                     }
+                     alert(data); 
+                     $('#frmCorte').action='?o=11';
+                     $('#frmCorte').submit();
+                     //Ocultar modal form
+                  });
+                  
+              }
+              $('#forma-agrega-corte' ).dialog( 'close' );
+         }
+         
+             
          $( '#forma-agrega-corte' ).dialog({
 			autoOpen: false,
 			height: 300,
 			width: 350,
-			modal: true,
+			modal: true/*,
 			buttons: {
-			    'Agregar corte':function(){
-                                if($("#fecha_ini").val()>$("#fecha_fin").val()){
-                                    alert('El fin de periodo debe ser posterior');
-                                }
-                                else{
-                                    var parameters={};
-                                    parameters.fecha_ini=$("#fecha_ini").val();
-                                    parameters.fecha_fin=$("#fecha_ini").val();
-                                    parameters.descripcion=$("#descripcion").val();
-                                    parameters.accion='agregar';
-                                    parameters.corte=$("#nuevo-corte").val();
-                                    $.post('Catalogos/Cortes/cortes.jsp', parameters, function(data){
-                                       //agregar la fila
-                                       if (data.indexOf('Agregado')){
-                                           var fila='<tr id="corte_'+parameters.corte+
-                                               '"><td class="resultado">'+parameters.corte+
-                                               '</td><td class="resultado">'+parameters.fecha_ini+
-                                               '</td><td class="resultado">'+parameters.fecha_fin+
-                                               '</td><td class="resultado">'+parameters.descripcion+
-                                               '</td><td class="centrado"><input type="checkbox" name="corte" value='+parameters.corte+'></td>'+
-                                               '</td><td class="centrado"><input type="radio" name="modificar" value='+parameters.corte+'></td>'+
-                                               '</tr></table>';
-                                           //escribir fila
-                                           $( '#info-cortes tbody' ).append(fila);
-                                           $( this ).dialog( 'close' );
-                                       }
-                                       //avisar 
-                                       alert(data); 
-                                    },'text'   );
-                               // },'json'
-                                }//else
-                            }//function
-                        }//button,
+			    'Agregar corte':agregar,
+                            'Modificar corte':modificar
+                     }//buttons*/
                    });
           
-          $('#btn-agrega-corte').click(function(){
-                     $('#forma-agrega-corte').dialog('open');
-                     $("#fecha_fin").datepicker({altFormat: 'dd-mm-yyyy'});
-                     $("#fecha_ini").datepicker({altFormat: 'dd-mm-yyyy'});
-           });
-           
- 
-           
+         $('#btn-agrega-corte').click(function(){
+               var $d= $('#forma-agrega-corte');
+              $d.dialog("option","buttons",{'Guardar':agregar});
+              $d.dialog("option", "title", "Agregar Corte");
+              $d.dialog('open');
+              $('#opcion').val('agregar');
+              console.log('opcion seteado a agregar');
+              $("#fecha_fin").datepicker({altFormat: 'dd/mm/yy'});
+              $("#fecha_ini").datepicker({altFormat: 'dd/mm/yy'});
          });
+           
+         
+         $('#btn-cambia-corte').click(
+          function(){
+            var radio = $('input[@id=modificar]:checked');
+            var corte=radio.val();
+            var param=getValores(corte);
+            setValores(param, 'modificar');
+            var $d= $('#forma-agrega-corte');
+            $d.dialog("option","buttons",{'Guardar':modificar});
+            $d.dialog("option", "title", "Modificar Corte");
+            $d.dialog('open');
+            $('#opcion').val('modificar');
+            console.log('opcion seteado a modificar');
+            $("#fecha_fin").datepicker({altFormat: 'dd/mm/yy'});
+            $("#fecha_ini").datepicker({altFormat: 'dd/mm/yy'});
+          });
+             
+        $('#btn-borra-corte').click(function(){
+            $('#opcion').val('borrar');
+        });
+           
+        $('button').addClass("ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only");
+         
+      
+  });
--- web/Catalogos/Cortes/cortes.jsp
+++ web/Catalogos/Cortes/cortes.jsp
@@ -1,6 +1,7 @@
 <%@page import="java.lang.StringBuffer" %>
 <%@page import="java.sql.*, javax.servlet.*"%>
-<% String accion=request.getParameter("accion");
+<% 
+   String accion=request.getParameter("accion");
    Connection conect = (Connection)session.getAttribute("conn");
    conect.setAutoCommit(true);
    java.sql.Statement stm = conect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
@@ -18,11 +19,41 @@
    }
    corte=corte==null?"1":corte;
    rst.close();
-   rst=stm.executeQuery("select corte, date_format(fecha_ini,'%d/%m/%Y')fecha_ini, date_format(fecha_fin,'%d/%m/%Y')fecha_fin, descripcion from cortes");
+   
+
+    rst = stm.executeQuery("SELECT date_format(d,'%d') max_fecha_fin_d, date_format(d,'%m') max_fecha_fin_m, date_format(d,'%Y') max_fecha_fin_y FROM (SELECT max(fecha_fin) d FROM cortes) mxd");
+    String max_fecha_fin_d = null ;
+    String max_fecha_fin_m = null ;
+    String max_fecha_fin_y = null ;
+    if (rst.first()){
+       max_fecha_fin_d = rst.getString("max_fecha_fin_d");
+       max_fecha_fin_m = rst.getString("max_fecha_fin_m");
+       max_fecha_fin_y = rst.getString("max_fecha_fin_y");
+       // TODO como no es posible que no tenga fecha fin, agregar constraints a las fechas
+   }
+     /*else{
+       // TODO que pasa si no hay registros
+     }*/
+   rst.close();     
+   
+   
+   rst=stm.executeQuery("select corte, date_format(fecha_ini,'%d/%m/%Y')fecha_ini, "+
+           "date_format(fecha_fin,'%d/%m/%Y')fecha_fin, descripcion from cortes");
+   
+   PreparedStatement pst = conect.prepareStatement(" select * from tc_grupos where corte=?");
+   
+     
              
 %>
 <script src="Catalogos/Cortes/cortes.js" ></script>
+<br>
     <center>
+        <button class="ui-button" id="btn-agrega-corte">Iniciar corte</button> 
+        <%if(max_fecha_fin_d!=null){%>
+            <button class="ui-button" id="btn-borra-corte">Borrar corte</button> 
+            <button class="ui-button" id="btn-cambia-corte">Cambiar corte</button> 
+        <%}%>    
+        <br>
         <table id="info-cortes">
         <thead>
             <th colspan="6">Cortes actuales</th>
@@ -37,20 +68,39 @@
                 <th>Modificar</th>
             </tr>
             <%if(rst.first()){
-                do{%>
-            <tr id="corte_<%=rst.getString("corte")%>">
-                <td class="resultado"><%=rst.getString("corte")%></td>
-                <td class="resultado"><%=rst.getString("fecha_ini")%></td>
-                <td class="resultado"><%=rst.getString("fecha_fin")%></td>
-                <td class="resultado"><%=rst.getString("descripcion")%></td>
-                <td class="centrado">
-                    <input type="checkbox" name="corte" id="corte" value="<%=rst.getString("corte")%>"/>
-                </td>
-                <td class="centrado">
-                    <input type="radio" name="modificar" id="modificar" value="<%=rst.getString("corte")%>"/>
-                </td>
-            </tr>
-            <%}while(rst.next());
+                ResultSet rst2=null;
+                boolean primero=true;
+                String cte=null;
+                do{
+                 cte=rst.getString("corte");%>
+                <tr id="corte_<%=cte%>">
+                    <td class="resultado"><%=rst.getString("corte")%></td>
+                    <td class="resultado">
+                        <%=rst.getString("fecha_ini")%>
+                        <input type="hidden" id="fecha_ini_<%=cte%>" value="<%=rst.getString("fecha_ini")%>"/>
+                    </td>
+                    <td class="resultado">
+                        <%=rst.getString("fecha_fin")%>
+                        <input type="hidden" id="fecha_fin_<%=cte%>" value="<%=rst.getString("fecha_fin")%>"/>
+                    </td>
+                    <td class="resultado">
+                        <%=rst.getString("descripcion")%>
+                        <input type="hidden" id="descripcion_<%=cte%>" value="<%=rst.getString("descripcion")%>"/>
+                    </td>
+                    <td class="centrado">
+                        <% pst.setString(1, cte);
+                           rst2=pst.executeQuery();
+                           if(!rst2.first()){%>
+                            <input type="checkbox" name="corte" id="corte" value="<%=rst.getString("corte")%>"/>
+                        <%}
+                          rst2.close();%>
+                    </td>
+                    <td class="centrado">
+                        <input type="radio" <%=primero?"checked":""%>   name="modificar" id="modificar" value="<%=rst.getString("corte")%>"/>
+                    </td>
+                </tr>
+                <%primero=false;
+                }while(rst.next());
               }
               else{%>
               <tr>
@@ -58,13 +108,7 @@
               </tr>
             <%}%>
         </tbody>
-        
     </table>
-    
-    <button class="ui-button" id="btn-agrega-corte">Iniciar corte</button> 
-    <button class="ui-button" id="btn-borra-corte">Borrar corte</button> 
-    <button class="ui-button" id="btn-cambia-corte">Cambiar corte</button> 
-    
       <div id="forma-agrega-corte" >
         <form id="frmCorte" method="post">
             <fieldset >
@@ -76,10 +120,15 @@
                   <p class="etiqueta"><label>Descripcion:</label>  
                 </div>      
                 <div>
-                 <p> <input type="hidden" id="nuevo-corte" value="<%=corte%>"/><%=corte%><br>
+                 <p> <input type="hidden" id="nuevo-corte" value="<%=corte%>"/>
+                     <label id="lbl_corte"><%=corte%></label><br>
                  <p><input type="text" id="fecha_ini" size="10" readonly/>
                  <p><input type="text" id="fecha_fin" size="10" readonly />
                  <p><textarea id="descripcion"></textarea>
+                    <input type="hidden" id="opcion"/>
+                    <input type="hidden" id="max_fecha_fin_d" value="<%=max_fecha_fin_d!=null?max_fecha_fin_d:"01/01/1990"%>"/>
+                    <input type="hidden" id="max_fecha_fin_m" value="<%=max_fecha_fin_m!=null?max_fecha_fin_m:"01/01/1990"%>"/>
+                    <input type="hidden" id="max_fecha_fin_y" value="<%=max_fecha_fin_y!=null?max_fecha_fin_y:"01/01/1990"%>"/>
                 </div>
             </fieldset>
         </form>   
@@ -92,13 +141,19 @@
      
      /*borrar elemento*/
      if(accion.equalsIgnoreCase("borrar")){
-            out.println("OK");
+         out.println("OK");
      }
      
      /*modificar elemento*/
-     if(accion.equalsIgnoreCase("modificar")){
-         out.println("OK");
+     if(accion.equalsIgnoreCase("modificar")){%>
+     <%@include file="cambiarCorte.jspf"%>
+     <%}
+      //cerrar statements y resultsets 
+     try{
+       if(rst!=null)rst.close();
+       if(stm!=null)stm.close();
      }
-  }%>
+     catch(Exception ex){}finally{}
+}%>
 
 
